fc28fbda566073a8f96d0b0c17a3f24ee27793cf
HiveTrending <hive_up@protonmail.com>
2021-09-04 13:08:48 -0700

    Update help text for gif command

--- pizza-discord-bot.py
+++ pizza-discord-bot.py
@@ -368,7 +368,7 @@ async def price(ctx, symbol=TOKEN_NAME):
 @bot.command()
 @commands.guild_only()
 async def gif(ctx, category=''):
-    """ Drop a random GIF! """
+    """ Drop a random GIF! Categories: pizza, bro, risingstar, pob, profound, battleaxe, englang, huzzah, beard, lego, blurt"""
 
     gif_set = PIZZA_GIFS
 
