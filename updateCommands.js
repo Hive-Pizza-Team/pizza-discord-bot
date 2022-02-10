6238770e2521f8e4e50d39a3c37abbd94a5f868c
HiveTrending <hive_up@protonmail.com>
2022-02-10 14:16:54 -0800

    add new slash command for search

--- updateCommands.js
+++ updateCommands.js
@@ -1,10 +1,12 @@
 const fetch = require('node-fetch')
 const dotenv = require('dotenv').config()
+const sleep = require('sleep-promise');
 
 const apiEndpoint = process.env.COMMANDS_API_ENDPOINT
 const botToken = process.env.DISCORD_TOKEN
 
-const gifCommandData = {
+var allCommandData = [
+/*{
   "name": "gif",
   "description": "Show a gif!",
   "options": [{
@@ -24,10 +26,8 @@ const gifCommandData = {
       {"name": "dibblers", value: "dibblers"}
     ]
   }]
-}
-
-
-const hkplayerCommandData = {
+},
+{
   "name": "hkplayer",
   "description": "Show HashKings player info.",
   "options": [{
@@ -36,49 +36,18 @@ const hkplayerCommandData = {
     "description": "name of player",
     "required": true
   }]
-}
-
-
-async function main () {
-
-  var response = await fetch(apiEndpoint, {
-    method: 'get',
-    headers: {
-      'Authorization': 'Bot ' + botToken,
-      'Content-Type': 'application/json'
-    }
-  })
-  var json = await response.json()
-  //console.log(response)
-  console.log(json)
-
-  response = await fetch(apiEndpoint, {
-    method: 'post',
-    body: JSON.stringify(gifCommandData),
-    headers: {
-      'Authorization': 'Bot ' + botToken,
-      'Content-Type': 'application/json'
-    }
-  })
-  json = await response.json()
-
-  //console.log(response)
-  console.log(json)
-
-    response = await fetch(apiEndpoint, {
-    method: 'post',
-    body: JSON.stringify(hkplayerCommandData),
-    headers: {
-      'Authorization': 'Bot ' + botToken,
-      'Content-Type': 'application/json'
-    }
-  })
-  json = await response.json()
-
-  //console.log(response)
-  console.log(json)
-
-  const rcCommandData = {
+},
+{
+  "name": "prefix",
+  "description": "Admin-only - print and change bot's command prefix.",
+  "options": [{
+    "type": 3,
+    "name": "newprefix",
+    "description": "new prefix to set",
+    "required": false
+  }]
+},
+{
   "name": "rc",
   "description": "Show Hive resource credits status for wallet.",
   "options": [{
@@ -87,20 +56,267 @@ async function main () {
     "description": "name of wallet",
     "required": true
   }]
+},
+{
+  "name": "witness",
+  "description": "Print Hive Witness Info.",
+  "options": [{
+    "type": 3,
+    "name": "witnessname",
+    "description": "name of witness",
+    "required": false
+  }]
+},
+{
+  "name": "hewitness",
+  "description": "Print Hive-Engine Witness Info.",
+  "options": [{
+    "type": 3,
+    "name": "witnessname",
+    "description": "name of witness",
+    "required": false
+  }]
+},
+{
+  "name": "dluxnodes",
+  "description": "Check DLUX Nodes Status."
+},
+{
+  "name": "sellbook",
+  "description": "Check Hive-Engine sell book for token.",
+  "options": [{
+    "type": 3,
+    "name": "symbol",
+    "description": "Hive-Engine token symbol. i.e. PIZZA.",
+    "required": false
+  }]
+},
+{
+  "name": "buybook",
+  "description": "Check Hive-Engine buy book for token.",
+  "options": [{
+    "type": 3,
+    "name": "symbol",
+    "description": "Hive-Engine token symbol. i.e. PIZZA.",
+    "required": false
+  }]
+},
+{
+  "name": "top10",
+  "description": "Print Hive-Engine token rich list top 10.",
+  "options": [{
+    "type": 3,
+    "name": "symbol",
+    "description": "Hive-Engine token symbol. i.e. PIZZA.",
+    "required": false
+  }]
+},
+{
+  "name": "tokenomics",
+  "description": "Print Hive-Engine token distribution info.",
+  "options": [{
+    "type": 3,
+    "name": "symbol",
+    "description": "Hive-Engine token symbol. i.e. PIZZA.",
+    "required": false
+  }]
+},
+{
+  "name": "price",
+  "description": "Print Hive-Engine / CoinGecko market price info.",
+  "options": [{
+    "type": 3,
+    "name": "symbol",
+    "description": "Hive-Engine token symbol. i.e. PIZZA.",
+    "required": false
+  }]
+},
+{
+  "name": "history",
+  "description": "Print Hive-Engine market trade history.",
+  "options": [{
+    "type": 3,
+    "name": "symbol",
+    "description": "Hive-Engine token symbol. i.e. PIZZA.",
+    "required": false
+  }]
+},
+{
+  "name": "blog",
+  "description": "Link to latest post from blog.",
+  "options": [{
+    "type": 3,
+    "name": "name",
+    "description": "Hive blog name, i.e. hive.pizza.",
+    "required": true
+  }]
+},
+{
+  "name": "bal",
+  "description": "Print Hive-Engine wallet balances.",
+  "options": [{
+    "type": 3,
+    "name": "wallet",
+    "description": "Hive wallet name, i.e. hive.pizza.",
+    "required": true
+  },{
+    "type": 3,
+    "name": "symbol",
+    "description": "Hive-Engine token symbol, i.e. PIZZA.",
+    "required": false
+  }]
+},
+{
+  "name": "bals",
+  "description": "Print Hive-Engine wallet balances.",
+  "options": [{
+    "type": 3,
+    "name": "wallet",
+    "description": "Hive wallet name, i.e. hive.pizza.",
+    "required": true
+  }]
+},
+{
+  "name": "info",
+  "description": "Print Hive.Pizza project link."
+},
+{
+  "name": "links",
+  "description": "Use these links to support Hive.Pizza."
+},
+{
+  "name": "pools",
+  "description": "Check Hive-Engine DIESEL Pool Balances for Wallet.",
+  "options": [{
+    "type": 3,
+    "name": "wallet",
+    "description": "Hive wallet name, i.e. hive.pizza.",
+    "required": true
+  }]
+},
+{
+  "name": "pool",
+  "description": "Check Hive-Engine DIESEL Pool Info.",
+  "options": [{
+    "type": 3,
+    "name": "pool",
+    "description": "DIESEL Pool symbol pair, i.e. SWAP.HIVE:PIZZA",
+    "required": false
+  }]
+},
+{
+  "name": "poolrewards",
+  "description": "Check Hive-Engine DIESEL Pool Rewards Info.",
+  "options": [{
+    "type": 3,
+    "name": "pool",
+    "description": "DIESEL Pool symbol pair, i.e. SWAP.HIVE:PIZZA",
+    "required": false
+  }]
+},
+{
+  "name": "exodecards",
+  "description": "Get a player's Exode card collection info.",
+  "options": [{
+    "type": 3,
+    "name": "player",
+    "description": "Name of player, i.e. thebeardflex.",
+    "required": true
+  }]
+},
+{
+  "name": "apr",
+  "description": "Calculate approx. APR for HP delegation.",
+  "options": [{
+    "type": 3,
+    "name": "delegation_amount",
+    "description": "Amount of HP delegated.",
+    "required": true
+  },{
+    "type": 3,
+    "name": "poolsize",
+    "description": "Size of daily rewards pool.",
+    "required": false
+  }]
+},
+{
+  "name": "rsplayer",
+  "description": "Check Rising Star Player Stats.",
+  "options": [{
+    "type": 3,
+    "name": "player",
+    "description": "Name of player, i.e. thebeardflex.",
+    "required": true
+  }]
+},
+{
+  "name": "status",
+  "description": "Print bot's status information."
+},
+{
+  "name": "sl",
+  "description": "Fetch Splinterlands info for player or PIZZA guilds.",
+  "options": [{
+    "type": 3,
+    "name": "subcommand",
+    "description": "player, guild, brawl",
+    "required": true
+  },{
+    "type": 3,
+    "name": "arg",
+    "description": "Name of player, status, timer.",
+    "required": true
+  }]
+},*/
+{
+  "name": "search",
+  "description": "Search for Hive content.",
+  "options": [{
+    "type": 3,
+    "name": "query",
+    "description": "Search query",
+    "required": true
+  },{
+    "type": 3,
+    "name": "sort",
+    "description": "How to filter search results.",
+    "required": false,
+    "choices": [
+      {"name": "newest", value: "neweset"},
+      {"name": "relevance", value: "relevance"}
+    ]
+  }]
 }
+]
 
-    response = await fetch(apiEndpoint, {
-    method: 'post',
-    body: JSON.stringify(rcCommandData),
+
+async function main () {
+
+  /*var response = await fetch(apiEndpoint, {
+    method: 'get',
     headers: {
       'Authorization': 'Bot ' + botToken,
       'Content-Type': 'application/json'
     }
   })
-  json = await response.json()
-
+  var json = await response.json()
   //console.log(response)
-  console.log(json)
+  console.log(json)*/
 
+  for (let offset in allCommandData) {
+      response = await fetch(apiEndpoint, {
+      method: 'post',
+      body: JSON.stringify(allCommandData[offset]),
+      headers: {
+        'Authorization': 'Bot ' + botToken,
+        'Content-Type': 'application/json'
+      }
+    })
+    json = await response.json()
+
+    //console.log(response)
+    console.log(json)
+    await sleep(4000); // Wait 4000 ms
+  }
 }
 main()
