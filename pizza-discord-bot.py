54c801220f0b2d193242508c94e61836d62bf4bc
HiveTrending <62359919+hiveuprss@users.noreply.github.com>
2025-11-19 05:16:48 -0800

    fix price check

--- pizza-discord-bot.py
+++ pizza-discord-bot.py
@@ -248,8 +248,8 @@ def get_coin_price(coin='hive'):
 
 async def update_bot_user_status(bot):
     """Update the bot's status."""
-    last_price = float(get_market_history(symbol="HIVE")[-1]['price'])
-    last_price_usd = round(get_coin_price()[0] * last_price, 3)
+    # last_price = float(get_market_history(symbol=DEFAULT_TOKEN_NAME)[-1]['price'])
+    last_price_usd = round(get_coin_price()[0], 3)
 
     # from datetime import datetime
     # date_obj = datetime.fromisoformat('2025-11-19T13:00:00+00:00')
